6d2a37ed7a64ab9a37b91fe05e10316a8e72c9c3
clueless810 <cjuric@ASUSTUFDASHF15>
2024-10-15 14:25:58 -0700

    Password Program

---
+++ src/passwordInput.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include "password_input.h"
+
+
+void getPassword(char* password) {
+
+	// Struct for terminal settings
+	struct termios oldt, newt;
+
+	//Get current terminal settings and save them to oldt
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+
+	//Diasble echo in terminal (clear ECHO tag)
+	newt.c_lflag &= ~ECHO;
+
+	//Set the new terminal settings 
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+	cout << "Enter password: ";
+
+	//Read the user password
+	int index = 0;
+	char ch;
+	while (index < MAX_PASSWORD_LENGTH - 1) {
+		ch = getchar(); //read single character
+
+		//Break  when enter is pressed
+		if (ch == '\n' || ch == '\r') {
+			break;
+		}
+
+		//Handle backspace
+		if (ch == '\b' && index > 0) {
+			cout << "\b \b"; //moves cursor and erases character
+			index--;
+		} else {
+			password[index++] = ch;
+			cout << '*'; //Print an asterisk in place of each character
+		}
+	}
+
+	password[index] = '\0'; //null-terminate the password
+
+	//Restore the original terminal settings to show password
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+	cout << endl; //Move to the next line after password input
+}
---
+++ src/passwordInput.h
@@ -0,0 +1,14 @@
+
+#ifndef PASSWORD_INPUT_H
+#define PASSWORD_INPUT_H
+
+using namespace std;
+
+//password length
+#define MAX_PASSWORD_LENGTH 100
+
+//func dec
+void getPassword(char* password);
+
+#endif
+
